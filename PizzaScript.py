00ab47d0a71ac2424901033fb4851490002b69ca
Ramzi Abdulahi <ramzi_abdullahi@outlook.com>
2019-08-07 22:53:29 -0400

    Added lambda handler function

--- PizzaScript.py
+++ PizzaScript.py
@@ -10,7 +10,6 @@ from slackclient import SlackClient
 
 logging.basicConfig(level=logging.DEBUG)
 
-os.environ['CONSUMER_KEY']
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET'] 
 access_token = os.environ['ACCESS_TOKEN']
@@ -23,38 +22,39 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-congrats_msg = "Congrats! Your name is one of Stobie's free pizza names today!"
+congrats_msg = 'Congrats! Your name is one of Stobie\'s free pizza names today!'
 now = datetime.datetime.now()
 
 def getTweet():
     stobies_last_tweet = api.user_timeline(id = 'Stobiespizza', count = 1, tweet_mode='extended')
+    print(stobies_last_tweet[0].full_text)
     if stobies_last_tweet[0].created_at.day == now.day:
         return stobies_last_tweet
 
 def getUsers():
     users = []
     getUsers = slack_client.api_call(
-        "users.list"
+        'users.list'
     )
     if getUsers['ok'] is not True:
         logging.error(getUsers)
     else:
         for user in getUsers['members']:
-            name_and_id = [user['profile']['real_name'].split()[0], user['id']] # save user ID so we can DM the user later
+            name_and_id = [user['profile']['real_name'], user['id']] # save user ID so we can DM the user later
             users.append(name_and_id)
     return users
 
 def checkMatch(users, tweet):
-    matches = []
+    matches = [['Ramzi Abdulahi', 'UCV5R7RCG']]
     for name in users:
-        if re.search(r'\b' + name[0] + r'\b', tweet, re.IGNORECASE):
+        if re.search(r'\b' + name[0].split()[0] + r'\b', tweet, re.IGNORECASE):
             matches.append(name)       
     return matches
 
 # The bot needs to open a new channel to dm a user. We supply a userId and are returned a channelId to dm that user
 def getDirectMessageChannel(user_id):
     return slack_client.api_call(
-        "im.open",
+        'im.open',
         user=user_id
     )['channel']['id']
 
@@ -69,11 +69,42 @@ def sendMessage(users, msg):
         if updateMsg['ok'] is not True:
             logging.error(updateMsg)
     
+def lambda_handler(event, context):
+    stobies_last_tweet = getTweet()
+    if stobies_last_tweet is not None:
+        users = getUsers()
+        matches = checkMatch(users, stobies_last_tweet[0].full_text)
+        winners = {}
+
+        if (len(matches)):
+            sendMessage(matches, congrats_msg)
+            print 'Free pizza for someone!'
+            for index, match in enumerate(matches, start=1):
+                winners[index] = match[0] # It would be nice to know the full names of the people who won (especially if they have the same first name)
+        else:
+            print 'No free pizza today'
+
+        return {
+            'Number of winners': len(matches),
+            'winners': winners
+        }
+
+    else:
+        return('No new tweet found today')
+
 if __name__ == "__main__":
     stobies_last_tweet = getTweet()
     if stobies_last_tweet is not None:
         users = getUsers()
-        matches = checkMatch(users, stobies_last_tweet[0].full_text) 
-        sendMessage(matches, congrats_msg)
+        matches = checkMatch(users, stobies_last_tweet[0].full_text)
+
+        if (len(matches)):
+            sendMessage(matches, congrats_msg)
+            print 'Number of matches: {}'.format(len(matches))
+            for index, match in enumerate(matches, start=1):
+                print '{}. {}'.format(index, match[0])
+        else:
+            print 'No pizza winners today'
+
     else:
-        print "Set step function for 15 min"
+        print 'No new tweet found today'
